eb3f79d6b3d584bc3e08440a2c08a6c31145a5c5
Lazerfox86 <114264661+Lazerfox86@users.noreply.github.com>
2025-06-14 22:29:32 -0400

    server.py

--- src/server.py
+++ src/server.py
@@ -1,1 +1,10 @@
-print("Hello, PCB Generator!") 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, PCB Generator!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)  # Specify an open port
